b09833fa7379ff2cfb78ab6ebff11c20ad2b0d13
Thea Kim <kimmjnha@gmail.com>
2024-01-25 12:56:32 +0900

    breadMypage

--- RESOURCES/JS/sideBar.js
+++
@@ -1,8 +0,0 @@
-// sideBarToggle
-type = "text/javascript" >
-    $(document).ready(function () {
-        $(".sideBarDropdown ul.sub").hide();
-        $(".sideBarDropdown ul.sideBarDropdownToggle li").click(function () {
-            $("ul", this).slideToggle("normal");
-        });
-    });
